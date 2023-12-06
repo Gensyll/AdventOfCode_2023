e23d2aa271de46065b053693f0151fd75e22ccc3
Gensyll <Gensyll@users.noreply.github.com>
2023-12-05 22:37:07 -0500

    Initial commit

---
+++ day_1/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+    Advent of Code 2023 - Day 1
+    Matthew Wrobel
+    2023-12-03
+*/
+
+namespace AdventOfCode.Day1
+{
+    class Program{
+        static void Main(string[] args){
+            
+            //Part 1
+            //Read in the file
+            List<string> listOfInputs = File.ReadAllLines(Environment.CurrentDirectory + "\\input.txt").ToList();
+
+            int inputSum = 0;
+            foreach (string input in listOfInputs){
+                //Get list of gitis and add up the first/last combined
+                var digits = input.Where(chr => Char.IsDigit(chr)).ToArray(); 
+                inputSum += Int32.Parse(digits[0].ToString() + digits[digits.Length - 1].ToString());
+            }
+            Console.WriteLine("Part 1 Sum = " + inputSum);
+
+            //Part 2
+            string digit1, digit2;
+            int newInputSum = 0;
+            foreach (string input in listOfInputs){
+                digit1 = DetermineDigit(input);
+                digit2 = DetermineDigit(new string(input.Reverse().ToArray()), true);
+                newInputSum += Int32.Parse(digit1 + digit2);
+            }
+            Console.WriteLine("Part 2 Sum = " + newInputSum);
+            
+        }
+
+        static string DetermineDigit(string input, bool reverse = false){
+            var numbers = new Dictionary<string, int>(){
+                {"one", 1}, {"two", 2}, {"three", 3},
+                {"four", 4}, {"five", 5}, {"six", 6},
+                {"seven", 7}, {"eight", 8}, {"nine", 9},
+                {"zero", 0}
+            };
+
+            if(reverse){    //if reverse, flip the strings so we can work backwards
+                numbers = numbers.ToDictionary(x => new string(x.Key.Reverse().ToArray()), x => x.Value);
+            }
+            
+            int index = 0, digit = 0;
+            foreach(var character in input){
+                //Get input as span
+                var subInput = input.AsSpan(index++);
+                foreach(var n in numbers){
+                    //Look for key in span
+                    if(subInput.StartsWith(n.Key)){
+                        digit = n.Value;
+                        return digit.ToString();
+                    }
+                }
+                if((int)character >= 48 && (int)character <= 57){
+                    digit = ((int)character) - 48;
+                    break;
+                }
+            }
+            return digit.ToString();
+        }
+    }
+}
---
+++ day_2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Day2
+{
+    class Program{
+        const int TOT_R_CUBES = 12;
+        const int TOT_G_CUBES = 13;
+        const int TOT_B_CUBES = 14;     
+
+        struct Game{
+            public int id;
+            public int powerFewestCubes;
+        }   
+
+        static void Main(string[] args){
+            //Part 1
+            int sumOfIds = 0, sumOfPowers = 0;
+            List<string> listOfGames = File.ReadAllLines(Environment.CurrentDirectory + "\\input.txt").ToList();
+            foreach(string game in listOfGames){
+                Game? processedGame = ProcessGameString(game);
+                if(processedGame.HasValue){
+                    sumOfIds += processedGame.Value.id;
+                    //part2
+                    sumOfPowers += processedGame.Value.powerFewestCubes;
+                }
+            }
+
+            Console.WriteLine("Total sum of IDs = " + sumOfIds);
+            //Part 2
+            Console.WriteLine("Total sum of powers = " + sumOfPowers);
+        }
+
+        static Game? ProcessGameString(string gameInput){            
+            //Extract only game results
+            Match match = Regex.Match(gameInput, "(?<=Game.*:\\s+)(.*)");
+            List<string> cubePulls = match.Value.Split(";").ToList();
+
+            //part2 - determine fewest cube count
+            int fewBlue = -1, fewRed = -1, fewGreen = -1;
+            
+            //Go through and determine if each game is possible            
+            foreach(string roll in cubePulls){      
+                int blueCount = 0, redCount = 0, greenCount = 0;          
+
+                Match blueMatch = Regex.Match(roll, "(\\d+)(?= blue)");
+                Match redMatch = Regex.Match(roll, "(\\d+)(?= red)");
+                Match greenMatch = Regex.Match(roll, "(\\d+)(?= green)");
+
+                blueCount = blueMatch.Success ? Int32.Parse(blueMatch.Value) : 0;
+                redCount = redMatch.Success ? Int32.Parse(redMatch.Value) : 0;
+                greenCount = greenMatch.Success ? Int32.Parse(greenMatch.Value) : 0;
+                
+                if(blueCount > TOT_B_CUBES || greenCount > TOT_G_CUBES || redCount > TOT_R_CUBES)
+                    return null;
+
+                //part2
+                if((fewBlue == -1 || blueCount < fewBlue) && blueCount > 0) fewBlue = blueCount;
+                if((fewRed == -1 || redCount < fewRed) && redCount > 0) fewRed = redCount;
+                if((fewGreen == -1 || greenCount < fewGreen) && greenCount > 0) fewGreen = greenCount;
+
+            }
+
+            Game newGame = new Game();
+            newGame.id = Int32.Parse(gameInput.SkipWhile(c => !char.IsDigit(c)).TakeWhile(char.IsDigit).ToArray());
+            newGame.powerFewestCubes = fewBlue * fewRed * fewGreen;
+
+            return newGame;
+        }
+    }
+}
