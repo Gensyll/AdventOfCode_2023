caf7b33bc70d94b97bf68ba546dfd26b5445bc0e
Gensyll <Gensyll@users.noreply.github.com>
2023-12-09 01:49:39 -0500

    Day3Part1 progress

--- day_3/Day3.cs
+++ day_3/Day3.cs
@@ -1,8 +1,10 @@
 ﻿/*
-    Advent of Code 2023 - Day 1
+    Advent of Code 2023 - Day 3
     Matthew Wrobel
     2023-12-06
 */
+
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Day3
 {
@@ -30,7 +32,31 @@ namespace AdventOfCode.Day3
             //TODO: Determine part numbers
             for(int mapRow = 0; mapRow <= inputMap.GetLength(0) - 1; ++mapRow){
                 for(int mapCol = 0; mapCol <= inputMap.GetLength(1) - 1; ++mapCol){
-                    
+                    List<char> digitList = new List<char>();
+                    //Only proceed if current value is a digit
+                    if(Char.IsNumber(inputMap[mapRow, mapCol])){
+                        int mapColOffset = 0;
+                        //Get length of number for checking
+                        while(Char.IsNumber(inputMap[mapRow, mapCol + mapColOffset])){
+                            digitList.Add(inputMap[mapRow, mapCol + mapColOffset]);
+                            ++mapColOffset;
+                        }
+
+                        //Check spaces surrounding current number
+                        bool isAdjacent = false;
+                        for(int x = 0; x < mapColOffset; ++x){
+                            if(!char.IsLetterOrDigit(inputMap[mapRow, mapCol + x - 1]) && !inputMap[mapRow, mapCol + x - 1].Equals(".")) isAdjacent = true;
+                            if(!char.IsLetterOrDigit(inputMap[mapRow, mapCol + x]) && !inputMap[mapRow, mapCol + x].Equals(".")) isAdjacent = true;
+                            if(!char.IsLetterOrDigit(inputMap[mapRow, mapCol + x + 1]) && !inputMap[mapRow, mapCol + x + 1].Equals(".")) isAdjacent = true;
+                        }
+
+                        //Skip the digits we found
+                        mapCol += mapColOffset;
+                        foreach(char x in digitList){
+                            Console.Write(x);
+                        }
+                        Console.WriteLine();
+                    }
                     
                 }                
             } 
